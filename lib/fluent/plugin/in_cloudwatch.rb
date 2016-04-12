da9b309e2a8142b158f573f98583529a412ab74b
Martin E. Zulliger <mzulliger@gmail.com>
2016-04-12 17:49:01 +0200

    Addedd the possibility to change the key name

--- lib/fluent/plugin/in_cloudwatch.rb
+++ lib/fluent/plugin/in_cloudwatch.rb
@@ -132,9 +132,11 @@ class Fluent::CloudwatchInput < Fluent::Input
 
   def output
     @metric_name.split(",").each {|m|
-      name, s = m.split(":")
-      s ||= @statistics
+      name, s, key = m.split(":")
+      s = if not s or s.empty? then @statistics else s end
+      key = if not key or key.empty? then name else key end
       now = Time.now - @offset
+
       statistics = @cw.get_metric_statistics({
         :namespace   => @namespace,
         :metric_name => name,
@@ -150,10 +152,10 @@ class Fluent::CloudwatchInput < Fluent::Input
 
         # unix time
         catch_time = datapoint[:timestamp].to_i
-        router.emit(tag, catch_time, { name => data })
+        router.emit(tag, catch_time, { key => data })
       elsif @emit_zero
         time = Fluent::Engine.now
-        router.emit(tag, time, {name => 0})
+        router.emit(tag, time, {key => 0})
       else
         log.warn "cloudwatch: #{@namespace} #{@dimensions_name} #{@dimensions_value} #{name} #{s} datapoints is empty"
       end
